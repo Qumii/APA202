27cd214f083aca840b9570cd8a61490edbecc9e5
Qumii <gumralvt-apa202@code.edu.az>
2026-05-05 00:42:14 +0400

    mvc-intro

---
+++ 25-MVCIntro/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _25_MVCIntro.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index ()
+        {
+            return View();
+        }
+
+        public int? Detail(int? id)
+        {
+
+            if (id is null || id<1)
+            {
+                throw new Exception("Id sehvdir");
+            }
+            return id;  
+        }
+
+
+        public string Error()
+        {
+            return "error";
+        }
+    }
+}
