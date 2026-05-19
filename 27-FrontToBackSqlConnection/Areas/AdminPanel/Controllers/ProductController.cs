81816547ffbcf3fdf5791cf0b371f105107082e2
Qumii <gumralvt-apa202@code.edu.az>
2026-05-20 00:31:45 +0400

    ProductUpdate

--- 27-FrontToBackSqlConnection/Areas/AdminPanel/Controllers/ProductController.cs
+++ 27-FrontToBackSqlConnection/Areas/AdminPanel/Controllers/ProductController.cs
@@ -1,5 +1,8 @@
-﻿using _27_FrontToBackSqlConnection.Data;
+﻿using _27_FrontToBackSqlConnection.Areas.AdminPanel.ViewModels.Product;
+using _27_FrontToBackSqlConnection.Data;
+using _27_FrontToBackSqlConnection.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace _27_FrontToBackSqlConnection.Areas.AdminPanel.Controllers
 {
@@ -23,6 +26,34 @@ namespace _27_FrontToBackSqlConnection.Areas.AdminPanel.Controllers
         public async Task<IActionResult> Create()
         {
             return View();
+        }
+
+        [HttpPost]
+
+        //public async Task<IActionResult> Create(ProductCreateVM productCreateVM)
+        //{
+
+        //}
+         
+        public async Task<IActionResult> Update(int? id)
+        {
+
+            if (id is null || id < 1) return BadRequest();
+
+            Product? product = await _context.Products.FirstOrDefaultAsync(p=>p.Id==id);
+            if (product is null) return NotFound();
+
+            ProductUpdateVM productUpdateVM = new()
+            { 
+                Name = product.Name,
+                Price = product.Price,
+                SKU = product.SKU,
+                Description = product.Description,
+                CategoryId = product.CategoryId,
+                Categories= await _context.Categories.Where(c=> !c.IsDeleted).ToListAsync(),
+            };
+
+            return View(productUpdateVM);
         }
     }
 
--- 27-FrontToBackSqlConnection/Areas/AdminPanel/ViewModels/Product/ProductUpdateVM.cs
+++ 27-FrontToBackSqlConnection/Areas/AdminPanel/ViewModels/Product/ProductUpdateVM.cs
@@ -1,13 +1,14 @@
-﻿namespace _27_FrontToBackSqlConnection.Areas.AdminPanel.ViewModels
+﻿using _27_FrontToBackSqlConnection.Models;
 
+namespace _27_FrontToBackSqlConnection.Areas.AdminPanel.ViewModels.Product
 {
-    public class ProductCreateVM
+    public class ProductUpdateVM
     {
         public string Name { get; set; }
         public decimal Price { get; set; }
         public string Description { get; set; }
         public string SKU { get; set; }
         public int? CategoryId { get; set; }
-        
+        public List<Category>? Categories { get; set; }
     }
 }
